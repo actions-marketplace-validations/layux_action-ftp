2a91e7fcc20cb689345f25256e9665b2e32d69fc
MisterQuestions <misterquestions.mx@gmail.com>
2022-03-25 02:01:39 -0600

    feat: create input parser service

---
+++ src/input/application/action-input-parser.service.ts
@@ -0,0 +1,31 @@
+import * as core from '@actions/core';
+import YAML from 'yaml';
+
+import { ActionInput } from "src/input/domain/dtos/action-input.dto";
+import { Protocol } from '../domain/enums/protocol.enum';
+
+export class ActionInputParserService {
+  getActionInput() {
+    const actionInput = new ActionInput();
+
+    actionInput.protocol = core.getInput('protocol') as Protocol;
+    actionInput.host = core.getInput('host');
+    actionInput.port = parseInt(core.getInput('port'), 10);
+    actionInput.username = core.getInput('username');
+    actionInput.password = core.getInput('password');
+    actionInput.private_key = core.getInput('private_key');
+    actionInput.local_root = core.getInput('local_root');
+    actionInput.remote_root = core.getInput('remote_root');
+    actionInput.passive = core.getBooleanInput('passive');
+
+    core.setSecret(actionInput.password);
+    core.setSecret(actionInput.private_key);
+
+    const transfers = core.getInput('transfers');
+    const parsedTransfers = YAML.parse(transfers);
+
+    actionInput.transfers = parsedTransfers;
+
+    return actionInput;
+  }
+}
