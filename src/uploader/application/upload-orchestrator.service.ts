1b764a591a19680bc5b3969e6ecedf530dca10a3
MisterQuestions <misterquestions.mx@gmail.com>
2022-03-25 02:00:42 -0600

    feat: create base upload orchestrator

---
+++ src/uploader/application/upload-orchestrator.service.ts
@@ -0,0 +1,28 @@
+import { FileService } from './file.service';
+
+export class UploadOrchestratorService {
+  private readonly fileService = new FileService();
+
+  async getFilesToUpload(localPath: string, ignoreFiles: Array<string>) {
+    // If path is a directory when we want all the files inside it
+    // except the ones that are in ignoreFiles
+    const isDirectory = await this.fileService.isDirectory(localPath);
+
+    if (isDirectory) {
+      // Get all files from directory that
+    }
+
+    // If the path is a file directly then it makes sense to add it to the list
+    const isFile = await this.fileService.isFile(localPath);
+
+    if (isFile) {
+      // Return only the file
+    }
+
+    // Otherwise it may be a pattern to match files
+    // look for all files matching and return them in an array
+    
+  }
+
+
+}
