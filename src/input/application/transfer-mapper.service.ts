1a306e50985e2d601a452e569a1b25ee19d40385
Fernando Lopez <misterquestions.mx@gmail.com>
2022-03-28 11:59:39 -0600

    feat: add transfer debug

--- src/input/application/transfer-mapper.service.ts
+++ src/input/application/transfer-mapper.service.ts
@@ -3,10 +3,9 @@ import { Transfer as InputTransfer } from '../domain/dtos/transfer.dto';
 
 export class TransferMapperService {
   mapTransfers(transfers: Array<InputTransfer>): Array<Transfer> {
-    return transfers.map((transfer) => ({
+    return transfers.map<Transfer>((transfer) => ({
       forceClean: transfer.force_clean,
       ignore: transfer.ignore,
-      include: transfer.include,
       localPath: transfer.local_path,
       remotePath: transfer.remote_path,
       permissions: transfer.permissions,
