c02e9455b7f700979d11fa25e9b51fc9e9f60e28
MisterQuestions <misterquestions.mx@gmail.com>
2022-03-27 21:56:28 -0600

    feat: add logger for input validator

--- src/input/application/action-input-validator.service.ts
+++ src/input/application/action-input-validator.service.ts
@@ -1,8 +1,11 @@
-import { validate } from "class-validator";
+import { validate } from 'class-validator';
+import { LoggerFactory } from 'src/logger/application/logger.factory';
 
-import { ActionInput } from "../domain/dtos/action-input.dto";
+import { ActionInput } from '../domain/dtos/action-input.dto';
 
 export class ActionInputValidator {
+  private readonly logger = LoggerFactory.getLogger(ActionInputValidator.name);
+
   async validateActionInput(actionInput: ActionInput) {
     const validateErrors = await validate(actionInput, {
       forbidUnknownValues: true,
@@ -10,7 +13,10 @@ export class ActionInputValidator {
 
     if (validateErrors.length > 0) {
       const errorMessages = validateErrors.map((error) => error.toString());
+      this.logger.error(`Action input validation failed: ${errorMessages}`);
       throw new Error(errorMessages.join('\n'));
     }
+
+    this.logger.info('Action input validation passed');
   }
 }
